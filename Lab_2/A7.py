bf5d71063a1f64fe967cefe55b42a8ddbc240e4d
Матвеюк Антонина <Matvejk_AI_25@student.grsu.by>
2025-10-06 16:54:29 +0300

    Добавила проверку на допустимость значения вводимых переменных
    Размер шахматной доски 8х8, значит переменные не могут быть меньше нуля и больше восьми

--- Lab_2/A7.py
+++ Lab_2/A7.py
@@ -1,13 +1,24 @@
-x1 = int(input('Enter x1'))
-y1 = int(input('Enter y1'))
-x2 = int(input('Enter x2'))
-y2 = int(input('Enter y2'))
+x1 = int(input('Enter x1: '))
+y1 = int(input('Enter y1: '))
+x2 = int(input('Enter x2: '))
+y2 = int(input('Enter y2: '))
 
-color1 = 'White' if (x1 + y1) % 2 == 0 else 'Black'
-color2 = 'White' if (x2 + y2) % 2 == 0 else 'Black'
-
-if color1 == color2:
-    print("YES")
-    print(color1)
+if x1>8 or y1>8 or x2>8 or y2>8: print('Incorrect parameter value') 
 else:
-    print("NO")
+    if x1<0 or y1<0 or x2<0 or y2<0: print('Incorrect parameter value') 
+    else:
+        if (x1 + y1) % 2 == 0: 
+            color1 = 'White' 
+        else: 
+            color1 ='Black'
+
+        if (x2 + y2) % 2 == 0: 
+            color2 = 'White' 
+        else: 
+            color2 ='Black'
+
+        if color1 == color2:
+            print("YES")
+            print(color1)
+        else:
+            print("NO")
