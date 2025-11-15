427ccdd86ad1326f04304d3020898b59aec2d3f2
Матвеюк Антонина <Matvejk_AI_25@student.grsu.by>
2025-11-15 14:49:22 +0300

    Удалила вспомогательный print(sen)

--- Lab_5/A2.py
+++ Lab_5/A2.py
@@ -2,7 +2,7 @@ import re
 
 s = str(input('Enter sentense: '))
 sen = re.split(r'(?<=[.?!])\s+', s)
-print(sen)
 for sentences in sen:
     print(sentences)
 print(f'Amount of sentences: {len(sen)}')
+
