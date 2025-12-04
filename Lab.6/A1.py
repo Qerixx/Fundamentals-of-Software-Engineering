ba8cd156a1082a4bf7132a16a339b081e174baa0
Матвеюк Антонина <Matvejk_AI_25@student.grsu.by>
2025-12-04 19:52:27 +0300

    Загружено две функции чтения данных из файлов

---
+++ Lab.6/A1.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as ET
+
+
+def  load_users_data():
+    try:
+        tree_users = ET.parse('users.xml')
+        users_data =[]
+        for user in tree_users.findall('user'):
+            user_data ={
+                'user_id': int(user.find('user_id').text),
+                'name': (user.find('name').text),
+                'age': int(user.find('age').text),
+                'weight': int(user.find('weight').text),
+                'fitness_level': (user.find('fitness_level').text)}
+            users_data.append(user_data)
+        return users_data
+    except FileNotFoundError:
+        print("File not found")
+        return []
+
+def  load_workouts_data():
+    try:
+        tree_workouts = ET.parse('workouts.xml')
+        workouts_data =[]
+        for workout in tree_workouts.findall('workout'):
+            workout_data ={
+                'workout_id': int(workout.find('workout_id').text),
+                'user_id': int(workout.find('user_id').text),
+                'date': (workout.find('date').text),
+                'type': (workout.find('type').text),
+                'duration': int(workout.find('duration').text),
+                'distance': float(workout.find('distance').text),
+                'calories': int(workout.find('calories').text),
+                'avg_heart_rate': int(workout.find('avg_heart_rate').text),
+                'intensity': (workout.find('intensity').text)}
+            workouts_data.append(workout_data)
+        return workouts_data
+    except FileNotFoundError:
+        print("File not found")
+        return []
